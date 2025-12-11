bee61784828da2350b669a188161d46fdbab375f
udith444 <foggmaster99@gmail.com>
2025-12-11 17:32:57 +0530

    add user name to hello.c

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -7,13 +7,13 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "udith444: Hello, world\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "udith444: Goodbye, cruel world\n");
 }
 
 module_init(hello_init);
